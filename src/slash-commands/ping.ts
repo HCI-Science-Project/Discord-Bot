07d2862cbecd3b79796469706a311315ab182e4f
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-03-13 16:05:26 +0800

    format: prettier!

--- src/slash-commands/ping.ts
+++ src/slash-commands/ping.ts
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction } from 'discord.js'
 
 export const data = new SlashCommandBuilder()
-    .setName("ping") 
-    .setDescription("Replies with Pong!")
+	.setName('ping')
+	.setDescription('Replies with Pong!')
 
 export async function execute(interaction: CommandInteraction) {
-    await interaction.reply("pong!")
+	await interaction.reply('pong!')
 }
