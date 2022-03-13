2836e1fa93ac534ccd0a80704c432b52cc1ca75b
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-03-14 06:24:08 +0800

    added echo.ts (slash)

---
+++ src/slash-commands/echo.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption((option) =>
+		option
+			.setName('input')
+			.setDescription('The input to echo back')
+			.setRequired(true)
+	);
+
+export async function execute(interaction: CommandInteraction) {
+	// const input = interaction.options.getString('input');
+	await interaction.reply({
+		content: `you entered ${interaction.options.getString('input')}`,
+		ephemeral: false,
+	});
+}
