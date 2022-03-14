bdb22860389da7ac8ebf1ab0d10ac33ea99f7972
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-03-14 11:12:43 +0800

    fixed bad practice

--- src/text-commands/echo.ts
+++ src/text-commands/echo.ts
@@ -2,9 +2,6 @@ import { Message } from 'discord.js';
 import * as dotenv from 'dotenv';
 
 export async function execute(message: Message) {
-	const prefix = dotenv.config().parsed.PREFIX;
-	// WARN: bad practice to put '5'
-	// '5' is derived from 4 (length of command) + 1 (a space character)
-	const args = message.content.slice(prefix.length + 5);
-	await message.reply(`you entered: "${args}"`);
+	const input = message.content.split(' ').slice(1).join(' ');
+	await message.reply(`you entered: "${input}"`);
 }
