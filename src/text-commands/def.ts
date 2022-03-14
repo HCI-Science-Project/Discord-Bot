84d8d6e456b505d24be5f239dd45d78a8d302b3b
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-03-14 12:29:06 +0800

    def (text command): fix uppercase/lowercase

--- src/text-commands/def.ts
+++ src/text-commands/def.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js';
 export const defs = JSON.parse(JSON.stringify(require('../definitions.json')));
 
 export async function execute(message: Message) {
-	const input = message.content.split(' ').slice(1).join(' ');
+	const input = message.content.split(' ').slice(1).join(' ').toLowerCase();
 	const def = defs[input];
 	await message.reply(
 		def !== undefined
