8578937b4e2a418b2ff0e06c481a2aeb368ec494
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-03-14 12:26:22 +0800

    added def (text cmd)

---
+++ src/text-commands/def.ts
@@ -0,0 +1,12 @@
+import { Message } from 'discord.js';
+export const defs = JSON.parse(JSON.stringify(require('../definitions.json')));
+
+export async function execute(message: Message) {
+	const input = message.content.split(' ').slice(1).join(' ');
+	const def = defs[input];
+	await message.reply(
+		def !== undefined
+			? `**${input.toUpperCase()}**\n${defs[input]}`
+			: "Sorry, we don't have an entry for the term yet. Check back later!"
+	);
+}
