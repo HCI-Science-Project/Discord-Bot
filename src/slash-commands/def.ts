5d30cb4f748ce7edba0b338334a9c1f65bca2988
DET171 <mrgeek484@gmail.com>
2022-03-20 16:10:21 +0800

    yay

--- src/slash-commands/def.ts
+++ src/slash-commands/def.ts
@@ -2,6 +2,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 import Fuse from 'fuse.js';
+import Paginator from '../utils/paginator.js';
 
 // Parse the data from the JSON file.
 import defs from '../definitions.json';
@@ -17,20 +18,34 @@ export const data = new SlashCommandBuilder()
 	.setDescription('Replies with the definition of the term requested!')
 	.addStringOption((option) =>
 		option
-			.setName('input')
+			.setName('term')
 			.setDescription('The term you want to receive the definition of')
-			.setRequired(true),
+			.setRequired(false),
 	);
 
 // Reply to the user with the definition, if there is one.
 export async function execute(interaction: CommandInteraction) {
-	const input = interaction.options.getString('input').toLowerCase();
-	const def = fuse.search(input)[0]?.item;
-	await interaction.reply({
-		content:
-			def !== undefined
-				? `**${def?.item?.toUpperCase()}**\n${def?.definition}`
-				: 'Sorry, we don\'t have an entry for the term yet. Come back later!',
-		ephemeral: false,
-	});
+	const input = interaction.options.getString('term');
+	if (input) {
+		const def = fuse.search(input.toLowerCase())[0]?.item;
+		await interaction.reply({
+			content:
+				def !== undefined
+					? `**${def?.item?.toUpperCase()}**\n${def?.definition}`
+					: 'Sorry, we don\'t have an entry for the term yet. Come back later!',
+			ephemeral: false,
+		});
+	}
+	else {
+		const defsList = new Paginator(
+			defs.map((e) => ({
+				embeds: [{
+					title: e.item,
+					description: e.definition,
+				}],
+			})),
+		);
+
+		defsList.start({ interaction });
+	}
 }
---
+++ src/utils/paginator.d.ts
@@ -0,0 +1,56 @@
+// / <reference types="node" />
+export = Paginator;
+declare class Paginator {
+	/**
+     * @param {MessageEditOptions[]} data Array with edit options for each page.
+     * Creds to Melmsie and the dank memer devs
+     */
+	constructor(data: MessageEditOptions[]);
+	data: MessageEditOptions[];
+	currentPage: number;
+	row: MessageActionRow;
+	stopRow: MessageActionRow;
+	/**
+     * Starts the paginator.
+     * @param {object} options
+     * @param {CommandInteraction} options.interaction
+     * @param {number=} options.time
+     * @returns {Promise<Message>}
+     */
+	start({ interaction, time }: {
+        interaction: CommandInteraction;
+        time?: number | undefined;
+    }): Promise<Message>;
+    /**
+     * Listener for when a button is clicked.
+     * @param {ButtonInteraction} interaction
+     * @param {InteractionCollector} collector
+     * @returns {Promise<void>}
+     */
+	onClicked(interaction: ButtonInteraction, collector: InteractionCollector<any>): Promise<void>;
+	/**
+     * Listener for when the collector ends.
+     * @param {CommandInteraction} interaction
+     * @returns {Promise<void>}
+     */
+	onEnd(interaction: CommandInteraction): Promise<void>;
+	/**
+     * Gets the send options for a page.
+     * @param {number} number
+     */
+	getPage(number: number): {
+        components: MessageActionRow[];
+        attachments?: import('discord.js').MessageAttachment[];
+        content?: string;
+        embeds?: (import('discord-api-types').APIEmbed | import('discord.js').MessageEmbed | import('discord.js').MessageEmbedOptions)[];
+        files?: (import('stream').Stream | import('discord.js').BufferResolvable | import('discord.js').MessageAttachment | import('discord.js').FileOptions)[];
+        flags?: import('discord.js').BitFieldResolvable<import('discord.js').MessageFlagsString, number>;
+        allowedMentions?: import('discord.js').MessageMentionOptions;
+    };
+}
+import { MessageEditOptions } from 'discord.js';
+import { MessageActionRow } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
+import { Message } from 'discord.js';
+import { ButtonInteraction } from 'discord.js';
+import { InteractionCollector } from 'discord.js';
